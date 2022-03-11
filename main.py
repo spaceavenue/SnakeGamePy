b78c89f4111953139eef257ab5a18eefcc697538
sanyaxo <sanya.xo1337@gmail.com>
2022-03-11 23:32:35 +0530

    Added death screen message

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Fruit:
 
     def random(self):
            self.x = random.randint(0, block_num - 1)
-           self.y = random.randint(0, block_size - 1)
+           self.y = random.randint(0, block_num - 1)
            self.pos = pyg.math.Vector2(self.x, self.y)
 
 class Snake:
@@ -21,9 +21,7 @@ class Snake:
         self.body = [pyg.math.Vector2(5, 10), pyg.math.Vector2(4, 10)]
         self.direction = pyg.math.Vector2(1, 0)
         self.add_new = False
-        self.score_original = 0
-        self.score_new = 0
-        self.speed = 100
+        self.score = 0
 
     def drawSnake(self):
         for vec in self.body:
@@ -45,89 +43,98 @@ class Snake:
     def addVec(self):
         self.add_new = True
 
-#    def scoreCount(self):
-#        self.score_new += 1
-#        print(self.score_new)
-
-#    def gameSpeed(self):
-#        match (self.score_new - self.score_original):
-#                case 5:
-#                        self.speed -= 10
-#                        self.score_original = self.score_new
 class Main:
-    def __init__(self): 
+    def __init__(self):
         self.snake = Snake()
         self.fruit = Fruit()
-#        self.SCREEN_UPDATE = pyg.USEREVENT
+        self.font = pyg.font.SysFont("Arial", 14, bold=True, italic=False)
+
     def update(self):
-#        SCREEN_UPDATE = pyg.USEREVENT
-#        pyg.time.set_timer(self.SCREEN_UPDATE, self.snake.speed)
         self.snake.moveSnake()
         self.checkCollision()
-        self.gameSpeed()
         self.checkGameOver()
 
     def drawStuff(self):
         self.fruit.drawFruit()
         self.snake.drawSnake()
+        self.font_surface = self.font.render(str(self.snake.score), False, (255,255,255))
 
     def checkCollision(self):
         if self.fruit.pos == self.snake.body[0]:
             self.fruit.random()
             self.snake.addVec()
-            self.snake.score_new += 1
-
-    def gameSpeed(self):
-        if self.snake.score_new - self.snake.score_original == 1:
-            self.snake.speed -= 25
-            self.snake.score_original = self.snake.score_new
+            self.snake.score += 1
 
     def checkGameOver(self):
         if not 0 <= self.snake.body[0].x <= block_num or not 0 <= self.snake.body[0].y <= block_num:
-            self.gameOver()
+            raise "what"
+            
         for snek in self.snake.body[1:]:
             if snek == self.snake.body[0]:
-                self.gameOver()
+                raise "what"
+    
 
-    def gameOver():
-        pyg.quit()
+    def gameOver(self):
+        window.fill((0,0,0))
+        self.font = pyg.font.SysFont("Arial", 32, bold=True, italic=False)
+        self.death_message = self.font.render("You Died.", False, (255,255,255))
+        window.blit(self.death_message, (125, 125))
+        self.replay_input = self.font.render("Press Enter to play again, Esape to quit.", False, (255,255,255))
+        window.blit(self.replay_input, (150, 150))
+        pyg.display.update()
 
 pyg.init()
 block_num = 50
 block_size = 10
 window = pyg.display.set_mode((block_num * block_size, block_num * block_size))
+gameover_window = pyg.display.set_mode((block_num * block_size, block_num * block_size))
 
 mainGame = Main()
-screen_update = pyg.USEREVENT
-def thing(su, speed):
-#    SCREEN_UPDATE = pyg.USEREVENT
-    pyg.time.set_timer(su, speed)
+SCREEN_UPDATE = pyg.USEREVENT
+pyg.time.set_timer(SCREEN_UPDATE, 50)
+running = True
 
-while True:
-    thing(screen_update, mainGame.snake.speed)
-    for event in pyg.event.get():
-        if event.type == pyg.QUIT:
-            pyg.quit()
+try:
+    while running:
+    
+        for event in pyg.event.get():
 
-        elif event.type == SCREEN_UPDATE:
-            mainGame.update()
-            
-        elif event.type == pyg.KEYDOWN:
-            match event.key:
-                case pyg.K_UP:
-                    if mainGame.snake.direction.y != 1:
-                        mainGame.snake.direction = pyg.math.Vector2(0, -1)
-                case pyg.K_DOWN:
-                    if mainGame.snake.direction.y != -1:
-                        mainGame.snake.direction = pyg.math.Vector2(0, 1)
-                case pyg.K_LEFT:
-                    if mainGame.snake.direction.x != 1:
-                        mainGame.snake.direction = pyg.math.Vector2(-1, 0)
-                case pyg.K_RIGHT:
-                    if mainGame.snake.direction.x != -1:
-                        mainGame.snake.direction = pyg.math.Vector2(1, 0)
-                case pyg.K_ESCAPE:
-                    pyg.quit()
-    window.fill((0, 0, 0))
-    mainGame.drawStuff()
-    pyg.display.update()
+            if event.type == pyg.QUIT:
+                pyg.quit()
+
+            elif event.type == SCREEN_UPDATE:
+                mainGame.update()
+
+            elif event.type == pyg.KEYDOWN:
+                match event.key:
+                    case pyg.K_UP:
+                        if mainGame.snake.direction.y != 1:
+                            mainGame.snake.direction = pyg.math.Vector2(0, -1)
+                    case pyg.K_DOWN:
+                        if mainGame.snake.direction.y != -1:
+                            mainGame.snake.direction = pyg.math.Vector2(0, 1)
+                    case pyg.K_LEFT:
+                        if mainGame.snake.direction.x != 1:
+                            mainGame.snake.direction = pyg.math.Vector2(-1, 0)
+                    case pyg.K_RIGHT:
+                        if mainGame.snake.direction.x != -1:
+                            mainGame.snake.direction = pyg.math.Vector2(1, 0)
+                    case pyg.K_ESCAPE:
+                        pyg.quit()
+        
+        window.fill((0, 0, 0))
+        mainGame.drawStuff()
+        window.blit(mainGame.font_surface, (5,5))
+        pyg.display.update()
+
+except Exception as e:
+    while True:
+        for event in pyg.event.get():
+            if event.type == SCREEN_UPDATE:
+                mainGame.gameOver()
+            elif event.type == pyg.KEYDOWN:
+                match event.key:
+                    case pyg.K_RETURN:
+                        mainGame.__init__()
+                    case pyg.K_ESCAPE:
+                        pyg.quit()
